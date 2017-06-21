a5b9bd06ae80375acad8c910eb54f1b3b0581ee5
pand0rausa <pand0ra.usa@gmail.com>
2017-06-21 09:54:24 -0600

    Update IPdupRemove.py

--- IPdupRemove.py
+++ IPdupRemove.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
+
+# pip install ipcalc
+# python IPdupRemove.py 1.1.1.1/8
+
 import ipcalc
 import sys
 
 for x in ipcalc.Network(sys.argv[1]):
    print x
-pip install ipcalc first
-give it any cidr range it will print each individual ip in the range
-python cidrtoips.py 1.1.1.1/8 
+
